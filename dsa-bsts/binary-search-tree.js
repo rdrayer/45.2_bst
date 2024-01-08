71866528757cc79936238d3f60b013225ab7a921
Roberta Drayer <robertadrayer@gmail.com>
2024-01-07 18:36:47 -0800

    added insert functions

--- dsa-bsts/binary-search-tree.js
+++ dsa-bsts/binary-search-tree.js
@@ -15,14 +15,63 @@ class BinarySearchTree {
    * Returns the tree. Uses iteration. */
 
   insert(val) {
+    const newNode = new Node(val);
 
+    // if the tree is empty, set the new node as the root
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    // if the value already exists in the tree, don't allow dupes
+    while (true) {
+      if (val === current.val) {
+        return this;
+      }
+      if (val < current.val) {
+        // if there is no left child, insert new node as the left child
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        // move to the left child and continue the loop
+        current = current.left;
+      } else {
+        // if no right child, insert new node to right
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
   }
 
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
-  insertRecursively(val) {
+  insertRecursively(val, current = this.root) {
+    // If the tree is empty, insert at the root
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
+    }
 
+    if (val < current.val) {
+      if (current.left === null) {
+        current.left = new Node(val);
+        return this;
+      }
+      return this.insertRecursively(val, current.left);
+    } else {
+      if (current.right === null) {
+        current.right = new Node(val);
+        return this;
+      }
+      return this.insertRecursively(val, current.right);
+    }
   }
 
   /** find(val): search the tree for a node with value val.
